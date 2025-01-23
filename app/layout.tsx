d3d9419aeb3adfbc5735d72b61394a1058c3c643
yellowkk <yellowk4@hunet.co.kr>
2025-01-23 10:51:32 +0900

    수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
+import React from "react";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
